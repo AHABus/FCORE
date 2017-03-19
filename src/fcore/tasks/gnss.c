f0b2a2f5cbfad3027e8853e51e7b8ecbddf75786
Amy Parent <amy@amyparent.com>
2017-03-19 16:14:00 +0000

    Improve GNSS parsing to ensure GGA parsing
    Since GNSS receivers can broadcast other NMEA sentences than just
    GGA, we loop a finite amount of times until we find one. This
    ensures that we update the platform's position ever GNSS task
    tick.

--- src/fcore/tasks/gnss.c
+++ src/fcore/tasks/gnss.c
@@ -14,6 +14,8 @@
 #include "FreeRTOS.h"
 #include "task.h"
 
+#define GNSS_MAX_ATTEMPTS 4
+
 void fcore_gnssTask(void* parameters) {
     
     fcore_uartInit(9600);
@@ -22,27 +24,27 @@ void fcore_gnssTask(void* parameters) {
     
     while(true) {
         fcore_uartClear();
-        fcore_gnssReset();
         vTaskDelay(3000 / portTICK_PERIOD_MS);
         
-        GNSSStatus state = GNSS_PARSING;
-        while(fcore_uartAvailable() > 0 && state == GNSS_PARSING) {
-            state = fcore_gnssFeed(fcore_uartReadChar());
-        }
-        static char info[64];
-        uint32_t len = 0;
-        switch(state) {
-        case GNSS_VALID:
-            len = snprintf(info, 64, "valid NMEA packet {%d, %d, %d} (%d sats, %d)\r\n",
-                           FCORE.latitude, FCORE.longitude, FCORE.altitude, FCORE.satellites, FCORE.fixQuality);
-            fcore_rtxWrite((uint8_t*)info, len);
-            break;
-        case GNSS_NOTGGA:
-            fcore_rtxWrite((uint8_t*)"Not a GGA packet\r\n", strlen("Not a GGA packet\r\n"));
-            break;
-        default:
-            fcore_rtxWrite((uint8_t*)"Invalid NMEA packet\r\n", strlen("Invalid NMEA packet\r\n"));
-            break;
+        
+        for(int i = 0; i < GNSS_MAX_ATTEMPTS; ++i) {
+            
+            // Reset the GNSS NMEA parser
+            GNSSStatus state = GNSS_PARSING;
+            fcore_gnssReset();
+            
+            // Get the next sentence from UART0
+            while(fcore_uartAvailable() > 0 && state == GNSS_PARSING) {
+                state = fcore_gnssFeed(fcore_uartReadChar());
+            }
+            
+            if(state == GNSS_VALID) {
+                char info[64];
+                uint32_t len = snprintf(info, 64, "valid NMEA packet {%d, %d, %d} (%d sats, %d)\r\n",
+                                        FCORE.latitude, FCORE.longitude, FCORE.altitude, FCORE.satellites, FCORE.fixQuality);
+                fcore_rtxWrite((uint8_t*)info, len);
+                break;
+            }
         }
 
         vTaskDelay(10000 / portTICK_PERIOD_MS);
